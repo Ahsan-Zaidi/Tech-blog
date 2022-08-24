7abdfdfc5fcba8d0171e8c70cc833ca5d7bd7aff
Zaidi <zaidiahsan.r@gmail.com>
2022-08-24 15:22:20 -0500

    User class

---
+++ models/User.js
@@ -0,0 +1,38 @@
+//required files to create connection
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+//Create User class
+//model is only created once user information is verified
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+//Blueprint for User class
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [6]
+        }
+    }
+}, 
+{
+    
+
+})
