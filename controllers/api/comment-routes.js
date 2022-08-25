02c434a0d4906ff76c628aef61c30ed182437f0c
Zaidi <zaidiahsan.r@gmail.com>
2022-08-25 14:23:07 -0500

    Comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -25,3 +25,60 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
     })
 });
+
+//route to post a comment
+router.post('/', (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.body.user_id,
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    }
+});
+
+//route to update a comment
+router.put('/:id', (req, res) => {
+    Comment.update({
+        comment_text: req.body.comment_text
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found matching this id' });
+            return;
+        }
+        res.json(dbCommentData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//route to delete a comment
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found matching this id' });
+            return;
+        }
+        res.json(dbCommentData)
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
+
