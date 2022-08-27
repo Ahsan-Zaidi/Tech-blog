3a9a5f9e039ea093ed5fc8d9bd9e2d6cdd6c2a7d
Zaidi <zaidiahsan.r@gmail.com>
2022-08-27 17:44:52 -0500

    Comment javascript functionality

---
+++ public/java/comment.js
@@ -0,0 +1,34 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    //select the location of comment-text
+    const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    //awit api response to then input post and comment text data 
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        //if the response is acceptable load into id of form
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+            document.querySelector('#comment-form').style.display = "block";
+        }
+    }
+}
+
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
