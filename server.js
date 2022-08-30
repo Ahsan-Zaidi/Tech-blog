561c498a2afd7f1bc80708f8591a8c881ba70b9e
Zaidi <zaidiahsan.r@gmail.com>
2022-08-29 19:17:27 -0500

    test

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('public',express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(routes);
